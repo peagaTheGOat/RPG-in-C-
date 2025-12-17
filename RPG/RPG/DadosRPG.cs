d810125b8cf9c3215f723b6cb67e774b36c251f4
Pedro Henrique de Mello Sabino <pedrohenrique.mello.sabino@gmail.com>
2025-12-17 14:06:45 -0300

    Add files via upload

---
+++ RPG/RPG/DadosRPG.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+
+namespace RPG
+{
+    public class DadosRPG
+    {
+        public string Nome { get; set; }
+        public int PontosMana { get; set; }
+        public int PontosSaude { get; set; }
+        public void SalvarDados(DadosRPG[] dados, string caminhoArquivo)
+        {
+            var opcoes = new JsonSerializerOptions { WriteIndented = true };
+            string jsonString = JsonSerializer.Serialize(dados, opcoes);
+            File.WriteAllText(caminhoArquivo, jsonString);
+        }
+        public DadosRPG[] CarregarDados(string caminhoArquivo)
+        {
+            if (!File.Exists(caminhoArquivo))
+            {
+                throw new FileNotFoundException("Arquivo de dados não encontrado.", caminhoArquivo);
+            }
+            string jsonString = File.ReadAllText(caminhoArquivo);
+            DadosRPG[] dados = JsonSerializer.Deserialize<DadosRPG[]>(jsonString);
+            return dados ?? Array.Empty<DadosRPG>();
+        }
+
+    }
+}
